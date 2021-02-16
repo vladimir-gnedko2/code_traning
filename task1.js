69689318b3ac8ad30980eea9de65e08e80cba3fe
vladimir-gnedko2 <vavanchikmks@gmail.com>
2021-02-16 12:11:40 +0300

    chek

--- task1.js
+++ task1.js
@@ -1,6 +1,8 @@
 let add = 'Ask me aboUT earTh'
+let str = 'sHo Za hnYa'
 function repl(params) {
     let res = params.split('').map(item => item.toLowerCase()).join('')
     return res;
 }
 console.log(repl(add))
+console.log(repl(str))
